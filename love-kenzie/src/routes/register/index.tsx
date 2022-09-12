d458d30bead163e0afe4ce4268f0b93c6d723f9b
Mari Spinola <89092249+Mapinko@users.noreply.github.com>
2022-09-12 17:53:43 -0300

    feat: title component

--- love-kenzie/src/routes/register/index.tsx
+++ love-kenzie/src/routes/register/index.tsx
@@ -1,12 +1,20 @@
 import { Input } from '../../components/input'
+import { Title } from '../../components/title'
 
 export const RegisterPage = () => {
 	return (
-		<>
-			<Input placeholder='Name' />
-			<Input placeholder='Email' />
-			<Input placeholder='Password' />
-			<Input placeholder='Confirm Password' />
-		</>
+		<section>
+			<div>
+				<Title text='Create Account' />
+			</div>
+
+			<p>or use your email for registration:</p>
+			<div>
+				<Input placeholder='Name' />
+				<Input placeholder='Email' />
+				<Input placeholder='Password' />
+				<Input placeholder='Confirm Password' />
+			</div>
+		</section>
 	)
 }
