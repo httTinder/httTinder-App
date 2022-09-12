27b57131ed2f7c24f5ca474c27ebc29fa6c9dfd1
Mari Spinola <89092249+Mapinko@users.noreply.github.com>
2022-09-12 18:01:23 -0300

    feat: social images assets

--- love-kenzie/src/routes/register/index.tsx
+++ love-kenzie/src/routes/register/index.tsx
@@ -1,11 +1,23 @@
 import { Input } from '../../components/input'
 import { Title } from '../../components/title'
 
+import facebook from '../../assets/socials/facebook.svg'
+import mail from '../../assets/socials/mail.svg'
+
 export const RegisterPage = () => {
 	return (
 		<section>
 			<div>
 				<Title text='Create Account' />
+			</div>
+
+			<div>
+				<a>
+					<img src={mail} alt='Login with mail' />
+				</a>
+				<a>
+					<img src={facebook} alt='Login with facebook' />
+				</a>
 			</div>
 
 			<p>or use your email for registration:</p>
