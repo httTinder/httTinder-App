65b4ec6f46f0a5bdde16291685fb0645196d0f06
Mari Spinola <89092249+Mapinko@users.noreply.github.com>
2022-09-12 21:47:00 -0300

    feat: reset style

---
+++ love-kenzie/src/styles/global.ts
@@ -0,0 +1,65 @@
+import { createGlobalStyle } from 'styled-components'
+
+export default createGlobalStyle`
+     :root{
+          --purple: #9747FF ;
+          --pink: #F299B9 ;
+          --white: #FFFFFF ;
+          --red: #D7234D;
+
+     
+     }
+
+   * {
+          margin:0;
+          padding: 0;
+          outline:0;
+          box-sizing: border-box;
+     }
+
+     body,html{
+          width: 100vw;
+          height: 100vh;
+     }
+     
+     body {
+          overflow-x: hidden;
+     }
+     
+     h1{
+          font-family: 'Lobster Two', cursive;
+     }
+     
+     h2, button{
+          font-family: 'Secular One', sans-serif;
+     }
+     
+     p, li, input{
+          font-family: 'Didact Gothic', sans-serif;
+     }
+
+     button {
+          cursor:  pointer;
+     }
+
+       /* width */
+     ::-webkit-scrollbar {
+          width: 10px;
+     }
+
+     ::-webkit-scrollbar-track {
+          background: var(--color-text-light);
+          margin: 1rem;
+          border-radius: 1.6rem;
+     }
+
+       /* Handle */
+     ::-webkit-scrollbar-thumb {
+          background: #F2A30B;
+          border-radius: 1.6rem;
+     }
+     /* Handle on hover */
+     ::-webkit-scrollbar-thumb:hover {
+          background: #F2A30B;
+     }
+`
